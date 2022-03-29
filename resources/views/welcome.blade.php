4a83e85ac490e00b4f973625aa404b0662324af9
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-03-29 16:37:54 -0300

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,11 +14,20 @@ Welcome | Links
     <div class="row mt-5 text-center">
         <div class="col-2"></div>
         <div class="col-8">
-            <a href="{{ route('cliente_cadastro') }}" class="btn btn-primary">Cadastro cliente</a>
-            <a href="{{ route('clientes_listar') }}" class="btn btn-primary">Listas de cliente</a>
-            <a href="{{ route('produto_cadastro') }}" class="btn btn-primary">Cadastro Produto</a>
-            <a href="{{ route('produto_listar') }}" class="btn btn-primary">Listas de produtos</a>
-            <a href="{{ route('fornecedor_cadastro') }}" class="btn btn-primary">Cadastro Fornecedores</a>
+            <div class="row">
+                <a href="{{ route('cliente_cadastro') }}" class="btn btn-primary mb-3">Cadastro cliente</a>
+                <a href="{{ route('clientes_listar') }}" class="btn btn-primary mb-3">Lista de cliente</a>
+            </div>
+            <hr>
+            <div class="row">
+                <a href="{{ route('produto_cadastro') }}" class="btn btn-primary mb-3">Cadastro Produto</a>
+                <a href="{{ route('produto_listar') }}" class="btn btn-primary mb-3">Lista de produtos</a>
+            </div>
+            <hr>
+            <div class="row">
+                <a href="{{ route('fornecedor_cadastro') }}" class="btn btn-primary mb-3">Cadastro Fornecedores</a>
+                <a href="{{ route('fornecedor_listar') }}" class="btn btn-primary mb-3">Listar Fornecedores</a>
+            </div>
         </div>
         <div class="col-2"></div>
     </div>    
