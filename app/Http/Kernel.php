a0b36c4a51d55c611e207ee1bf5af8bd7ad6e304
Luís Felippe Tomazini Fernandes <37600658+EutruTepit@users.noreply.github.com>
2022-05-02 21:11:51 -0300

    Validação/midware
    Plus mais alguns testes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -63,5 +63,7 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'verifica.nivel' => \App\Http\Middleware\VerificaNivel::class,
+        'verifica.idade' => \App\Http\Middleware\VerificaIdade::class,
     ];
 }
